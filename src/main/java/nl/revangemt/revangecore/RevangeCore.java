aa1f572af0cce5f65cab874e2d90548aefaf05fe
TheBathDuck <35238334+TheBathDuck@users.noreply.github.com>
2022-05-18 19:06:02 +0200

    Module setup

--- src/main/java/nl/revangemt/revangecore/RevangeCore.java
+++ src/main/java/nl/revangemt/revangecore/RevangeCore.java
@@ -1,17 +1,18 @@
 package nl.revangemt.revangecore;
 
+import nl.revangemt.revangecore.modules.ModuleLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RevangeCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        ModuleLoader.load();
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
---
+++ src/main/java/nl/revangemt/revangecore/modules/ModuleLoader.java
@@ -0,0 +1,10 @@
+package nl.revangemt.revangecore.modules;
+
+import org.bukkit.Bukkit;
+
+public class ModuleLoader {
+
+    public static void load() {
+        Bukkit.getLogger().info("Loading modules..");
+    }
+}

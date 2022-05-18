aaae9f6d4f5206d3590248e6ad46f073e2017551
TheBathDuck <35238334+TheBathDuck@users.noreply.github.com>
2022-05-18 18:52:44 +0200

    Project Setup

---
+++ src/main/java/nl/revangemt/revangecore/RevangeCore.java
@@ -0,0 +1,17 @@
+package nl.revangemt.revangecore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RevangeCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

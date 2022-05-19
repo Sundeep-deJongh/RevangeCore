404d3069953f476b8fbfb7f2a836c5e94d0a3390
TheBathDuck <35238334+TheBathDuck@users.noreply.github.com>
2022-05-19 18:34:37 +0200

    Implemented Vanish logic.

--- src/main/java/nl/revangemt/revangecore/modules/playermodule/listeners/JoinListener.java
+++ src/main/java/nl/revangemt/revangecore/modules/playermodule/listeners/JoinListener.java
@@ -2,6 +2,7 @@ package nl.revangemt.revangecore.modules.playermodule.listeners;
 
 import nl.revangemt.revangecore.modules.restartmodule.RestartModule;
 import nl.revangemt.revangecore.utils.Utils;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,10 +11,13 @@ public class JoinListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
-        event.setJoinMessage(Utils.color("&7[&a+&7] " + event.getPlayer().getName()));
+        Player player = event.getPlayer();
+        if(!Utils.isVanished(event.getPlayer())) {
+            event.setJoinMessage(Utils.color("&7[&a+&7] " + event.getPlayer().getName()));
+        }
 
         if(RestartModule.getRestartBar() != null) {
-            RestartModule.getRestartBar().addPlayer(event.getPlayer());
+            RestartModule.getRestartBar().addPlayer(player);
         }
 
     }
--- src/main/java/nl/revangemt/revangecore/utils/Utils.java
+++ src/main/java/nl/revangemt/revangecore/utils/Utils.java
@@ -1,11 +1,21 @@
 package nl.revangemt.revangecore.utils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.MetadataValue;
+import sun.jvm.hotspot.oops.Metadata;
 
 public class Utils {
 
     public static String color(String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+
+    public static boolean isVanished(Player player) {
+        for(MetadataValue meta : player.getMetadata("vanished")) {
+            if(meta.asBoolean()) return  true;
+        }
+        return false;
     }
 
 }

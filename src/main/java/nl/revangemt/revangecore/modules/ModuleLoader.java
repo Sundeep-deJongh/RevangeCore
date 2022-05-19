4269d9d7548e88ba801a0aa7d2429e095a2207f7
MeneerEzraCooleDev <ezraschutte227@gmail.com>
2022-05-19 21:55:24 +0200

    Zieke taakstraf map gepushed

--- src/main/java/nl/revangemt/revangecore/modules/ModuleLoader.java
+++ src/main/java/nl/revangemt/revangecore/modules/ModuleLoader.java
@@ -4,18 +4,21 @@ import lombok.Getter;
 import nl.revangemt.revangecore.RevangeCore;
 import nl.revangemt.revangecore.modules.playermodule.PlayerModule;
 import nl.revangemt.revangecore.modules.restartmodule.RestartModule;
+import nl.revangemt.revangecore.modules.taakstrafmodule.TaakstrafModule;
 import org.bukkit.Bukkit;
 
 public class ModuleLoader {
 
     private static @Getter PlayerModule playerModule;
     private static @Getter RestartModule restartModule;
+    private static @Getter TaakstrafModule taakstrafModule;
 
     public static void load(RevangeCore plugin) {
         Bukkit.getLogger().info("Loading modules..");
 
         playerModule = new PlayerModule(plugin);
         restartModule = new RestartModule(plugin);
+        taakstrafModule = new TaakstrafModule(plugin);
     }
 
     public static void disable() {
